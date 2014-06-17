d0a006c318e124260a16b05c1a22517d41299568
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-06-17 12:28:09 +0200

    fixed problems with geo chart update on data/transform change

--- client/components/geo.js
+++ client/components/geo.js
@@ -4,39 +4,38 @@ var React = require("react");
 var d3 = require("d3");
 
 var Chart = function(props) {
+	// initial projection
+	var projection = d3.geo.mercator()
+		.scale(1)
+		.translate([ 0, 0 ]);
+
+	// create path
+	var path = d3.geo.path().projection(projection);
+
+	// proper object for calculating bounds
+	var multiPoints = {
+		"type": "MultiPoint",
+		"coordinates": props.data.map(function(object) {
+			return [ object.lat, object.lon ];
+		})
+	};
+
+	// calculate bounds, scale and translation
+	var bounds = path.bounds(multiPoints);
+	var scale = 0.98 / Math.max(
+		(bounds[1][0] - bounds[0][0]) / props.width,
+		(bounds[1][1] - bounds[0][1]) / props.height
+	);
+	var translate = [
+		(props.width - scale * (bounds[1][0] + bounds[0][0])) / 2,
+		(props.height - scale * (bounds[1][1] + bounds[0][1])) / 2
+	];
+
+	// update projection
+	projection.scale(scale).translate(translate);
+
 	// function for point update / create
-	var updatePoints = function(point) {
-		// initial projection
-		var projection = d3.geo.mercator()
-			.scale(1)
-			.translate([ 0, 0 ]);
-
-		// create path
-		var path = d3.geo.path().projection(projection);
-
-		// proper object for calculating bounds
-		var multiPoints = {
-			"type": "MultiPoint",
-			"coordinates": props.data.map(function(object) {
-				return [ object.lat, object.lon ];
-			})
-		};
-
-		// calculate bounds, scale and translation
-		var bounds = path.bounds(multiPoints);
-		var scale = 0.98 / Math.max(
-			(bounds[1][0] - bounds[0][0]) / props.width,
-			(bounds[1][1] - bounds[0][1]) / props.height
-		);
-		var translate = [
-			(props.width - scale * (bounds[1][0] + bounds[0][0])) / 2,
-			(props.height - scale * (bounds[1][1] + bounds[0][1])) / 2
-		];
-
-		// update projection
-		projection.scale(scale).translate(translate);
-
-		// finally update point
+	var updatePoint = function(point) {
 		point
 			.attr("transform", function(d) {
 				return "translate(" + projection([ d.lat, d.lon ]) + ")";
@@ -53,30 +52,32 @@ var Chart = function(props) {
 			.enter()
 			.append("circle")
 			.attr("class", "point")
-			.call(updatePoints);
+			.call(updatePoint);
 
 		points
 			.transition()
-			.call(updatePoints);
+			.call(updatePoint);
 	};
 };
 
 var GeoChart = React.createClass({
+	defaultWidth: 1100,
+	defaultHeight: 640,
+
 	updateChart: function(props) {
 		var chartProps = props || this.props;
-		console.log(chartProps);
+
+		chartProps.width = chartProps.width || this.defaultWidth;
+		chartProps.height = chartProps.height || this.defaultHeight;
 
 		d3.select(this.getDOMNode())
 			.call(Chart(chartProps));
 	},
 
 	render: function() {
-		this.props.width = this.props.width || 1100;
-		this.props.height = this.props.height || 640;
-
 		return React.DOM.svg({
-			"height": this.props.height,
-			"width": this.props.width
+			"height": this.props.height || this.defaultHeight,
+			"width": this.props.width || this.defaultWidth
 		});
 	},
 
